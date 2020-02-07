080b473147f9fceb8995d9613bd037c7f0f02c8b
Jonathan <jonathanwoollettlight@gmail.com>
2020-02-07 11:28:56 +0000

    Implemented better horizontal scaling

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 
 pub mod core {
-    pub fn line(min:f32,max:f32,data:&Vec<f32>,height:usize) {
+    pub fn line(min:f32,max:f32,data:&Vec<f32>,height:usize,h_spacing:usize) {
         
         if height < data.len() {
             panic!("`height` must be more than `(max-min).abs()`. Cannot squeeze data.");
@@ -42,20 +42,20 @@ pub mod core {
             vertical_scale(data,&inner_data,i);
 
             for t in 0..inner_data.len() {
-                //println!("inner_data[t]: {} <> {}",inner_data[t], i as f32);
-                print!(" ");
+                //println!("inner_data[t]: {} <&>= {}",inner_data[t], i as f32);
+                print!("{: <1$}","",h_spacing);
                 if inner_data[t] < i as f32 && inner_data[t] >= (i-1) as f32  {
-                    print!(" * ");
+                    print!("*");
                 }
                 else {
-                    print!("   ");
+                    print!(" ");
                 }
                 
             }
             
             println!();
         }
-        horizontal_scale(data,&inner_data);
+        horizontal_scale(data,&inner_data,h_spacing);
         println!();
 
         fn vertical_scale(data:&Vec<f32>,inner_data:&Vec<f32>,i:usize) {
@@ -73,16 +73,31 @@ pub mod core {
                 print!("       ");
             }
             print!("│");
-            print!("{: <1$}","",3);
         }
         // TODO Adjust this to be like the horizontal scale in my `splitter` library
-        fn horizontal_scale(data:&Vec<f32>,inner_data:&Vec<f32>) {
-            println!("       └{:─<1$}","",4usize*inner_data.len() + 4usize);
+        fn horizontal_scale(data:&Vec<f32>,inner_data:&Vec<f32>,h_spacing:usize) {
+            let v_axis_offset = 7usize;
+            print!("{: <1$}","",v_axis_offset);
+            println!("└{:─<1$}","",((inner_data.len()+1)*(h_spacing+1)) + 3);
 
             // Prints x-axis scale
-            print!("           ");
+            print!("{: <1$}","",v_axis_offset+1);
             for i in 0..inner_data.len() {
-                print!(" {:0>3}",i);
+                print!("{: <1$}","",h_spacing);
+                print!("{}",i%10);
+            }
+            print!("\n{: <1$}","",v_axis_offset+1);
+            for _ in 0..inner_data.len() / 10 {
+                print!("└{:─<1$}┘","",(10*(h_spacing+1))-2);
+            }
+            let remainder_labels = inner_data.len()%10;
+            let remainder_space = remainder_labels*(h_spacing+1);
+            print!("└{:─<1$}","",remainder_space);
+            print!("\n{: <1$}","",v_axis_offset+h_spacing);
+            print!("{: <1$}","",5*(h_spacing+1));
+            for i in 0..inner_data.len() / 10 {
+                print!("{:0>2}",i);
+                print!("{: <1$}","",(10*(h_spacing+1))-2);
             }
         } 
     }
@@ -97,7 +112,7 @@ mod tests {
         let data = vec![
             1f32,2f32,3f32,4f32,5f32,4f32,3f32,2f32,1f32
         ];
-        line(1f32,5f32,&data,10usize);
+        line(1f32,5f32,&data,10usize,0usize);
 
         assert!(false);
     }
@@ -109,7 +124,7 @@ mod tests {
         for val in &mut data {
             *val = -(*val);
         }
-        line(-5f32,-1f32,&data,10usize);
+        line(-5f32,-1f32,&data,10usize,2usize);
 
         assert!(false);
     }
@@ -123,7 +138,7 @@ mod tests {
             data.push((i as f32 * pi * eighth).sin());
         }
         //println!("data:\n{:.2?}",data);
-        line(-1f32,1f32,&data,33usize);
+        line(-1f32,1f32,&data,33usize,2usize);
 
         assert!(false);
     }
@@ -139,7 +154,7 @@ mod tests {
         }
         //println!("data:{:1.?}",data);
         //println!("data:\n{:.2?}",data);
-        line(-1f32,1f32,&data,33usize);
+        line(-1f32,1f32,&data,33usize,1usize);
 
         assert!(false);
     }
@@ -154,7 +169,7 @@ mod tests {
         }
         //println!("data:{:1.?}",data);
         //println!("data:\n{:.2?}",data);
-        line(-1f32,1f32,&data,41usize);
+        line(-1f32,1f32,&data,41usize,1usize);
 
         assert!(false);
     }
